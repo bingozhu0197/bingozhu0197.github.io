2e364526fff7afc57521c65eeb40a974145e8619
Zhu <binbin.zhu@nokia.com>
2016-11-06 16:57:54 +0800

    fix create list update

--- scripts/main.js
+++ scripts/main.js
@@ -43,7 +43,8 @@ function createEquation() {
 function createEquationList() {
     //equationListButton.style.display = "none";
     //startButton.style.display = "none";
-    record = [];
+    removeResult();
+    record = []
     for (i = 0; i < 60; i++)
     {
         createEquation();
